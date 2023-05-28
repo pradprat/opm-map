83f71788c323cc7018c3bfbcce6298b224019831
Pradika Fitra Pratama <pradikafp14@gmail.com>
2023-05-29 00:43:31 +0700

    add nvmrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,56 @@ import { useEffect } from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import mapboxgl from "mapbox-gl";
 import React from "react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
+import { Select } from "chakra-react-select";
 
 const IndexPage: React.FC<PageProps> = () => {
-  mapboxgl.accessToken = "pk.eyJ1IjoicHJhZHByYXQiLCJhIjoiY2tnMHhwbXZvMDc4eDJ1cXd1ZmFueHk5YiJ9.wfhci5Mpn6cahjx3GnOfYQ";
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoicHJhZHByYXQiLCJhIjoiY2tnMHhwbXZvMDc4eDJ1cXd1ZmFueHk5YiJ9.wfhci5Mpn6cahjx3GnOfYQ";
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: "mapbox", // container ID
-      style: "mapbox://styles/mapbox/streets-v12", // style URL
+      // mapbox light
+      style: "mapbox://styles/mapbox/light-v10", // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
-  
-    return () => {
-    }
-  }, [])
-  
+
+    return () => {};
+  }, []);
+
   return (
-    <div>
-      <div style={{ width: "100%", height: "100vh" }} id="mapbox"></div>
-    </div>
+    <Box>
+      {/* filters */}
+      <VStack
+        position={"absolute"}
+        bg={"white"}
+        p={6}
+        zIndex={1}
+        m={8}
+        borderRadius={"lg"}
+        shadow={"lg"}
+        alignItems={"flex-start"}
+        gap={4}
+      >
+        <Heading size={"md"}>Filter</Heading>
+        <Select
+          useBasicStyles
+          options={[
+            {
+              label: "I am red",
+              value: "i-am-red",
+              colorScheme: "red", // The option color scheme overrides the global
+            },
+            {
+              label: "I fallback to purple",
+              value: "i-am-purple",
+            },
+          ]}
+        ></Select>
+      </VStack>
+      <Box style={{ width: "100%", height: "100vh" }} id="mapbox"></Box>
+    </Box>
   );
 };
 
